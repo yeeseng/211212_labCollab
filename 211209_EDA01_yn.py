5b92474461e0246c5d1bb8463a1bc1ff344fa152
Yee Seng Ng <yeeseng82@gmail.com>
2022-01-30 09:22:50 -0600

    commit

--- 211209_EDA01_yn.py
+++ 211209_EDA01_yn.py
@@ -8,7 +8,19 @@ import statistics
 import glob
 import tqdm
 
-listOfYearlyImportFiles = glob.glob('Data/lab_data/*')
+listOfYearlyImportFiles = ['Data/lab_data/req1512_lab_2009.csv',
+                           'Data/lab_data/req1512_lab_2010.csv',
+                           'Data/lab_data/req1512_lab_2011.csv',
+                           'Data/lab_data/req1512_lab_2012.csv',
+                           'Data/lab_data/req1512_lab_2013.csv',
+                           'Data/lab_data/req1512_lab_2014.csv',
+                           'Data/lab_data/req1512_lab_2015.csv',
+                           'Data/lab_data/req1512_lab_2016.csv',
+                           'Data/lab_data/req1512_lab_2017.csv',
+                           'Data/lab_data/req1512_lab_2018.csv',
+                           'Data/lab_data/req1512_lab_2019.csv',
+                           'Data/lab_data/req1512_lab_2020.csv']
+
 print(len(listOfYearlyImportFiles))
 print(listOfYearlyImportFiles)
 
@@ -23,10 +35,8 @@ for eachIndex, eachRow in componentDataDF.iterrows():
             listOfLabComponents.append(eachRow[eachColumnName])
 
 print('number of lab components:', len(listOfLabComponents))
-
-print(listOfLabComponents[250])
-
-listOfLabComponents=listOfLabComponents[250:]
+#print(listOfLabComponents[250])
+#listOfLabComponents=listOfLabComponents[250:]
 
 # function to preprocess list to remove nonsensical inputs and get only numerical values
 def filterList(oldList):
@@ -63,40 +73,43 @@ def analyzeList(thisList):
     except:
         thisStdev = -1
         
-    return {'count':len(thisList),'min':thisMin, 'max':thisMax, 'mean':thisMean, 'stdev':thisStdev}
+    return {'acceptedCount':len(thisList),'min':thisMin, 'max':thisMax, 'mean':thisMean, 'stdev':thisStdev}
+
+paramList = ['totalCount', 'acceptedCount', 'min', 'max', 'mean', 'stdev']
 
 # generate string
 def genString(componentName, paramDict):
     thisString = componentName.strip(',')
-    thisString +=','+str(paramDict['count'])
-    thisString +=','+str(paramDict['min'])
-    thisString +=','+str(paramDict['max'])
-    thisString +=','+str(paramDict['mean'])
-    thisString +=','+str(paramDict['stdev'])
+    for eachParam in paramList:
+        thisString +=','+str(paramDict[eachParam])
     thisString +='\n'
     return thisString
 
-
-# In[ ]:
-
-
 # For EDA, to figure out which column was the ex
 with open("outputs/results.csv", "a") as text_file:
-    text_file.write('component name,count,min,max,mean,stdev\n')
+    thisString = 'component name'
+    for eachParam in paramList:
+        thisString += ',' + eachParam
+    thisString += '\n'
+    text_file.write(thisString)
 
 for eachComponent in listOfLabComponents:
     print(eachComponent)
     completeListOfORDvalues = []
+    totalCount = 0
     for eachYearlyImportFile in listOfYearlyImportFiles:
         workingDataFrame = pd.read_csv(eachYearlyImportFile, usecols = ['COMMON_NAME','COMPONENT_NAME','BASE_NAME','ORD_VALUE','REFERENCE_UNIT'])
         workingDataFrame['ORD_VALUE'] = workingDataFrame['ORD_VALUE'].astype('str') 
         workingDataFrame['nameMatched'] = workingDataFrame.apply(lambda x: x['COMMON_NAME']==eachComponent, axis=1)
         workingDataFrame = workingDataFrame[workingDataFrame['nameMatched']==True]
         eachYearListOfORDvalues = workingDataFrame['ORD_VALUE'].values
+        totalCount += len(eachYearListOfORDvalues) # get count of cases before processing
         eachYearListOfORDvalues = filterList(eachYearListOfORDvalues)
         print(len(eachYearListOfORDvalues))
         completeListOfORDvalues += eachYearListOfORDvalues
         print(len(completeListOfORDvalues))
     with open("outputs/results.csv", "a") as text_file:
-        stringToSave = genString(eachComponent,analyzeList(completeListOfORDvalues))
+        resultDict = analyzeList(completeListOfORDvalues)
+        resultDict['totalCount'] = totalCount
+        stringToSave = genString(eachComponent,resultDict)
         text_file.write(stringToSave)
