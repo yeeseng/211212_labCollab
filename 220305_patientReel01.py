e57e2b00ff148fab21a086eb91ff8cffc6ff0cc2
Yee Seng Ng <yeeseng82@gmail.com>
2022-04-09 17:16:11 -0500

    commit

--- 220305_patientReel01.py
+++ 220305_patientReel01.py
@@ -13,6 +13,14 @@ def parseResultTime(resultTime):
 def calcReelFrame(resultDateString, referenceTime=datetime(2008, 9, 30)):
     return parseResultTime(resultDateString)-referenceTime
 
+def normalizeHelper(ORD_VALUE, FINAL_REF_LOW, FINAL_REF_HIGH):
+    normalizedValue = 0
+    try:
+        normalizedValue = (float(ORD_VALUE)-float(FINAL_REF_LOW))/(float(FINAL_REF_HIGH)-float(FINAL_REF_LOW))-0.5
+    except:
+        normalizedValue = 0
+    return normalizedValue
+
 # NOTE: needs enc from #2 in table of contents where hospital encounters are excluded
 def lab_clean(df, use_ref_ranges=True):
     # Restrict to non-hospital through inner join
@@ -35,20 +43,24 @@ def lab_clean(df, use_ref_ranges=True):
         df['FINAL_REF_LOW'] = df.apply(lambda x: x.REF_LOW_2 if ~np.isnan(x.REF_LOW_2) else x.REFERENCE_LOW, axis=1)
         df['FINAL_REF_HIGH'] = df.apply(lambda x: x.REF_HIGH_2 if ~np.isnan(x.REF_HIGH_2) else x.REFERENCE_HIGH, axis=1)
         df = df[(~df.FINAL_REF_LOW.isna()) & (~df.FINAL_REF_HIGH.isna())]
+        df['ORD_VALUE_NORM'] = df.apply(lambda x: normalizeHelper(ORD_VALUE = x.ORD_VALUE, FINAL_REF_LOW=x.FINAL_REF_LOW, FINAL_REF_HIGH=x.FINAL_REF_HIGH), axis=1)
     return df
 
-def singleLabComponentReel(df, labComp='WBC'):
+def singleLabComponentReel(df, labComp):
+    #print(labComp)
     thisDF = df[df['COMMON_NAME']==labComp]
-    labREEL = np.zeros(200)
-    for eachIndex in range(200):
-        listOfLabs = df[df['REEL_FRAME']==eachIndex].ORD_VALUE.values
-        listOfLabs = [float(eachItem) for eachItem in listOfLabs]
-        if len(listOfLabs)>0:
-            labREEL[eachIndex] = statistics.mean(listOfLabs)
-        elif eachIndex ==0:
-            labREEL[eachIndex]=0
-        else:
-            labREEL[eachIndex]=labREEL[eachIndex-1]
+    #print(thisDF.head())
+    labREEL = np.zeros(580)
+    if len(thisDF)>0:
+        for eachIndex in range(580):
+            listOfLabs = thisDF[thisDF['REEL_FRAME']==eachIndex].ORD_VALUE_NORM.values
+            listOfLabs = [float(eachItem) for eachItem in listOfLabs]
+            if len(listOfLabs)>0:
+                labREEL[eachIndex] = statistics.mean(listOfLabs)
+            elif eachIndex ==0:
+                labREEL[eachIndex]=0
+            else:
+                labREEL[eachIndex]=labREEL[eachIndex-1]
     return labREEL
 
 if __name__ == "__main__":
@@ -56,11 +68,15 @@ if __name__ == "__main__":
     listOfIDs = list(set([eachItem.split('_')[0] for eachItem in listOfFileNames]))
     listOfIDs.sort(reverse=True)
 
-    csvFilepathTemplate = 'Data/lab_data_patientLvl/id_*.csv'
+    labComponentsDF = pd.read_csv('Data/lab_stats_Yee_JW_edit.csv')
+    labCompList = labComponentsDF[labComponentsDF['include']==1]['component name'].values
 
-    print(len(listOfIDs))
-    for eachID in listOfIDs[4:5]:
-        print(eachID)
+    csvFilepathTemplate = 'Data/lab_data_patientLvl/id_*.csv'
+    npySavePath = 'Data/lab_data_patientReels/id.npy'
+
+    #print(len(listOfIDs))
+    for eachID in tqdm.tqdm(listOfIDs):
+        #print(eachID)
         thisPath = csvFilepathTemplate.replace('id',eachID)
         thisPathList = glob.glob(thisPath)
         thisListOfDataFrames = [pd.read_csv(eachItem, usecols=['MRN', 'PAT_ENC_CSN_ID', 'ORDER_PROC_ID', 'PROC_NAME', 'RESULT_DATE', 'COMMON_NAME',
@@ -70,9 +86,19 @@ if __name__ == "__main__":
 
         idDataFrame = pd.concat(thisListOfDataFrames)
         idDataFrame = lab_clean(idDataFrame)
-        labCompList = ['HEMATOCRIT']
+        #print(idDataFrame.dtypes)
+
+        labReelList = []
         for eachItem in labCompList:
             labReel = singleLabComponentReel(idDataFrame, labComp=eachItem)
-            print(labReel)
+            labReelList.append(labReel)
+        patientReel = np.stack(labReelList)
+
+        thisSavePath = npySavePath.replace('id', eachID)
+        #print(thisSavePath)
+        np.save(thisSavePath, patientReel)
+
+        #print(labReelList.shape)
+        #print(labReelList[:,579].shape)
 
         #print(thisListOfDataFrames)
