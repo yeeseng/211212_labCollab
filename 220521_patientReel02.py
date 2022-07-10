158f92ebe7d455677e904ba2089b11e55ca0e1ca
Yee Seng Ng <yeeseng82@gmail.com>
2022-07-09 20:48:34 -0500

    commit

--- 220521_patientReel02.py
+++ 220521_patientReel02.py
@@ -36,6 +36,7 @@ def lab_clean(df, use_ref_ranges=True):
     # Restrict to >9/2008
     df['REEL_FRAME'] = df.apply(lambda x: calcReelFrame(x['RESULT_DATE']).days//14, axis=1)
     df = df[df['REEL_FRAME'] > 0]
+
     # Remove external orders/results
     df = df[(df.ORDER_CLASS_C != 30) & (df.ORDER_CLASS_C != 143)]
     # Remove point of care
@@ -46,23 +47,27 @@ def lab_clean(df, use_ref_ranges=True):
         df['REF_LOW_2'] = df.REF_NORMAL_VALS.apply(lambda x: x.split('-')[0] if (isinstance(x, str)) else x)
         df['REF_HIGH_2'] = df.REF_NORMAL_VALS.apply(
             lambda x: x.split('-')[1] if ((isinstance(x, str)) and (len(x.split('-')) == 2)) else x)
+        df.REFERENCE_LOW = pd.to_numeric(df['REFERENCE_LOW'], errors='coerce')
+        df.REFERENCE_HIGH = pd.to_numeric(df['REFERENCE_HIGH'], errors='coerce')
         df.REF_LOW_2 = pd.to_numeric(df['REF_LOW_2'], errors='coerce')
         df.REF_HIGH_2 = pd.to_numeric(df['REF_HIGH_2'], errors='coerce')
         df['FINAL_REF_LOW'] = df.apply(lambda x: x.REF_LOW_2 if ~np.isnan(x.REF_LOW_2) else x.REFERENCE_LOW, axis=1)
         df['FINAL_REF_HIGH'] = df.apply(lambda x: x.REF_HIGH_2 if ~np.isnan(x.REF_HIGH_2) else x.REFERENCE_HIGH, axis=1)
         df = df[(~df.FINAL_REF_LOW.isna()) & (~df.FINAL_REF_HIGH.isna())]
         df['ORD_VALUE_NORM'] = df.apply(lambda x: normalizeHelper(ORD_VALUE = x.ORD_VALUE, FINAL_REF_LOW=x.FINAL_REF_LOW, FINAL_REF_HIGH=x.FINAL_REF_HIGH), axis=1)
-        #df['ORD_VALUE_NORM']= df.apply(lambda x: convertToFloat(x.ORD_VALUE), axis=1)
+    else:
+        df['ORD_VALUE_NORM']= df.apply(lambda x: convertToFloat(x.ORD_VALUE), axis=1)
 
     return df
 
 def singleLabComponentReel(df, labComp):
     #print(labComp)
-    thisDF = df[df['COMMON_NAME']==labComp]
+    #thisDF = df[df['COMMON_NAME']==labComp]
+    thisDF = df[df['COMMON_NAME'].isin(labComp)]
     #print(thisDF.head())
-    labREEL = np.zeros(240)
+    labREEL = np.zeros(380)
     if len(thisDF)>0:
-        for eachIndex in range(240):
+        for eachIndex in range(380):
             listOfLabs = thisDF[thisDF['REEL_FRAME']==eachIndex].ORD_VALUE_NORM.values
             listOfLabs = [float(eachItem) for eachItem in listOfLabs]
             if len(listOfLabs)>0:
@@ -74,46 +79,62 @@ def singleLabComponentReel(df, labComp):
     return labREEL
 
 if __name__ == "__main__":
+    with open('Data/completedReels.txt') as f:
+        idsOfCompletedReels = f.read().splitlines()
+
     enc = pd.read_csv('Data/req1512_encounter.csv')
 
     listOfFileNames = [eachItem.split('/')[-1][:-3] for eachItem in glob.glob('Data/lab_data_patientLvl/*.csv')]
     listOfIDs = list(set([eachItem.split('_')[0] for eachItem in listOfFileNames]))
+    print('total number of patient IDs:', len(listOfIDs))
+    print('Number of uncompleted reels:', len(idsOfCompletedReels))
+    listOfIDs = [eachItem for eachItem in listOfIDs if eachItem not in idsOfCompletedReels]
+    print('Number of uncompleted reels:', len(listOfIDs))
     listOfIDs.sort(reverse=True)
 
-    labComponentsDF = pd.read_csv('Data/lab_stats_Yee_JW_edit.csv')
-    labCompList = labComponentsDF[labComponentsDF['include']==1]['component name'].values
+    labComponentsDF = pd.read_csv('Data/52_lab_components_jw_04232022.csv')
+    labGroupList = labComponentsDF['group name'].unique()
 
     csvFilepathTemplate = 'Data/lab_data_patientLvl/id_*.csv'
     npySavePath = 'Data/lab_data_patientReels/id.npy'
 
     #print(len(listOfIDs))
-    for eachID in tqdm.tqdm(listOfIDs[0:100]):
-        #print(eachID)
-        thisPath = csvFilepathTemplate.replace('id',eachID)
-        thisPathList = glob.glob(thisPath)
-        thisListOfDataFrames = [pd.read_csv(eachItem, usecols=['MRN', 'PAT_ENC_CSN_ID', 'ORDER_PROC_ID', 'PROC_NAME', 'RESULT_DATE', 'COMMON_NAME',
-                                                'COMPONENT_NAME', 'BASE_NAME', 'ORD_VALUE', 'REFERENCE_LOW', 'REFERENCE_HIGH', 'REFERENCE_UNIT',
-                                                'REF_NORMAL_VALS', 'ORDER_CLASS_C', 'ORDER_CLASS'])
-                                for eachItem in thisPathList]
+    for eachID in tqdm.tqdm(listOfIDs):
+        try:
+            #print(eachID)
+            thisPath = csvFilepathTemplate.replace('id',eachID)
+            thisPathList = glob.glob(thisPath)
+            thisListOfDataFrames = [pd.read_csv(eachItem, usecols=['MRN', 'PAT_ENC_CSN_ID', 'ORDER_PROC_ID', 'PROC_NAME', 'RESULT_DATE', 'COMMON_NAME',
+                                                    'COMPONENT_NAME', 'BASE_NAME', 'ORD_VALUE', 'REFERENCE_LOW', 'REFERENCE_HIGH', 'REFERENCE_UNIT',
+                                                    'REF_NORMAL_VALS', 'ORDER_CLASS_C', 'ORDER_CLASS'])
+                                    for eachItem in thisPathList]
 
-        idDataFrame = pd.concat(thisListOfDataFrames)
-        idDataFrame = lab_clean(idDataFrame)
+            idDataFrame = pd.concat(thisListOfDataFrames)
 
-        print(idDataFrame)
+            if len(idDataFrame)>0:
+                idDataFrame = lab_clean(idDataFrame, use_ref_ranges=False)
 
-        #labCompList = ['CREATININE']
-        labReelList = []
-        for eachItem in labCompList:
-            labReel = singleLabComponentReel(idDataFrame, labComp=eachItem)
-            labReelList.append(labReel)
-        patientReel = np.stack(labReelList)
+            #labCompList = ['CREATININE']
+            labReelList = []
+            for eachItem in labGroupList:
+                listOfComponentNames = labComponentsDF[labComponentsDF['group name']==eachItem]['component name'].values
+                labReel = singleLabComponentReel(idDataFrame, labComp=listOfComponentNames)
+                labReelList.append(labReel)
+            patientReel = np.stack(labReelList)
 
-        thisSavePath = npySavePath.replace('id', eachID)
-        #print(thisSavePath)
-        np.save(thisSavePath, patientReel)
-        thisSavePath = thisSavePath.replace('npy', 'csv')
-        print(thisSavePath)
-        np.savetxt(thisSavePath, patientReel, delimiter=",")
+            thisSavePath = npySavePath.replace('id', eachID)
+            #print(thisSavePath)
+            np.save(thisSavePath, patientReel)
+            thisSavePath = thisSavePath.replace('npy', 'csv')
+            #print(thisSavePath)
+            np.savetxt(thisSavePath, patientReel, delimiter=",")
+
+            with open('Data/completedReels.txt', 'a') as file:
+                file.write(eachID+'\n')
+        except:
+            with open('Data/completedReels_withErrors.txt', 'a') as file:
+                file.write(eachID + '\n')
+
 
         #print(labReelList.shape)
         #print(labReelList[:,579].shape)
