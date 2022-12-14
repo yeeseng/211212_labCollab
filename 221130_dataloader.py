b0c7340166faf8a39c4bbd32a211a41a19444578
Yee Seng Ng <yeeseng82@gmail.com>
2022-12-14 05:40:47 -0600

    commit

--- 221130_dataloader.py
+++ 221130_dataloader.py
@@ -7,6 +7,7 @@ import tqdm
 from datetime import datetime, timedelta
 import random
 from torch.utils.data import Dataset as BaseDataset
+import random
 
 class labCollabDataset(BaseDataset):
     def __init__(
@@ -16,22 +17,41 @@ class labCollabDataset(BaseDataset):
             args=None
     ):
         self.dataframe = dataframe
-        self.patientList = dataframe['mrn'].to_list()
+        self.patientList = dataframe.index.to_list()
 
     def __getitem__(self, i):
         thisPatientMRN = self.patientList[i]
+        thisSeries = self.dataframe.loc[thisPatientMRN]
+
+        if thisSeries.loc['IDA']:
+            diagnosisReelFrame = thisSeries.loc['reelframe']-random.randint(6,12)
+        else:
+            diagnosisReelFrame = random.randint(1,350)-random.randint(6,12)
+
+        endFrame = max(diagnosisReelFrame,0)
+        startFrame = max(diagnosisReelFrame-12,0)
+        frameLength = endFrame-startFrame
+        offset = 12-frameLength
+
+        selectedReel = np.zeros((52,12))
+
         patientReel = np.load('Data/lab_data_patientReels/'+str(thisPatientMRN)+'.npy')
-        return patientReel
+        selectedReel[:, offset:] = patientReel[:,startFrame:endFrame]
+
+        return selectedReel, thisSeries.loc['IDA']
 
     def __len__(self):
         return len(self.patientList)
 
 if __name__ == "__main__":
-    dataDF = pd.read_csv('Data/Fe_def_outcome_cleanedAndStratified_YN.csv')
+    dataDF = pd.read_csv('Data/Fe_def_outcome_cleanedAndStratified_YN.csv', index_col='mrn')
     dataDF = dataDF[dataDF['blacklist']==False]
+    print(dataDF.head())
 
     thisDataset = labCollabDataset(dataframe=dataDF)
     print(thisDataset.__getitem__(3))
+
+    print()
     '''
     #listOfFiles = glob.glob('Data/lab_data_patientReels/*')
     #print(len(listOfFiles))
