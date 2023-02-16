a711f136eb777a383c82f7ca133d3efc0389eff6
Yee Seng Ng <yeeseng82@gmail.com>
2023-02-16 15:46:04 -0600

    commit

--- 100_basicModel01.py
+++ 100_basicModel01.py
@@ -25,6 +25,7 @@ import torchvision.datasets as dsets
 
 import torchmetrics
 import pytorch_lightning as pl
+from pytorch_lightning.loggers import WandbLogger
 
 class BiGRUver2(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers):
@@ -43,7 +44,7 @@ class BiGRUver2(nn.Module):
     def forward(self, x):
         batch_size = x.size()[0]
 
-        h0 = torch.zeros(self.num_layers * 2, batch_size, self.hidden_size)
+        h0 = torch.zeros(self.num_layers * 2, batch_size, self.hidden_size).cuda()
         # c0 = torch.zeros(self.num_layers * 2, x.size(0), self.hidden_size).cuda()
 
         # Forward pass
@@ -57,13 +58,35 @@ class BiGRUver2(nn.Module):
 
         return studyLevelOutputs
 
+class simpleANN(nn.Module):
+    def __init__(self, sequence_size, input_size):
+        super(simpleANN, self).__init__()
+        self.linear1 = nn.Linear(sequence_size*input_size, 312)
+        self.linear2 = nn.Linear(312, 156)
+        self.linear3 = nn.Linear(156, 1)
+
+    def forward(self, x):
+        x = torch.flatten(x, start_dim=1, end_dim=-1)
+
+        # Forward pass
+        x = F.relu(self.linear1(x))
+        x = F.relu(self.linear2(x))
+        x = F.relu(self.linear3(x))
+
+        return x
+
 class labCollabLM(pl.LightningModule):
     def __init__(self, params):
         super().__init__()
         self.save_hyperparameters(params)
         self.mainModel = BiGRUver2(input_size=52, hidden_size=32, num_layers=1)
+        #self.mainModel = simpleANN(sequence_size=12, input_size=52)
 
-        #Metrics
+        # Loss
+        self.criterion = torch.nn.BCEWithLogitsLoss()
+        #self.criterion = torch.nn.BCELoss()
+
+        # Metrics
         self.accuracy = torchmetrics.Accuracy(task='binary')
         self.learning_rate = self.hparams.lr
 
@@ -74,9 +97,10 @@ class labCollabLM(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         x, y = batch
         z_class = self.mainModel(x)
+        y_class = torch.sigmoid(z_class)
 
         # class loss
-        totalLoss = self.comp_loss(z_class, y)
+        totalLoss = self.criterion(z_class, y)
 
         self.log('train_totalLoss', totalLoss, on_step=True, on_epoch=True, prog_bar=True, logger=True, sync_dist=True)
         return totalLoss
@@ -84,12 +108,26 @@ class labCollabLM(pl.LightningModule):
     def validation_step(self, batch, batch_idx):
         x, y = batch
         z_class = self.mainModel(x)
+        y_class = torch.sigmoid(z_class)
 
-        totalLoss = self.comp_loss(z_class, y)
+        #print(y_class.dtype)
+        #print(y.dtype)
+        #breakpoint()
+
+        '''
+        if torch.sum(y)==0 or torch.sum(y)==self.hparams.batch_size:
+            totalLoss = 0
+            self.log('error', 1, on_step=False, on_epoch=True, prog_bar=True, logger=True, sync_dist=True, batch_size=self.hparams.batch_size)
+        else:
+            totalLoss = self.criterion(z_class, y)
+            self.log('error', 0, on_step=False, on_epoch=True, prog_bar=True, logger=True, sync_dist=True, batch_size=self.hparams.batch_size)
+        '''
+
+        totalLoss = self.criterion(z_class, y)
         self.log('valid_totalLoss', totalLoss, on_step=True, on_epoch=True, prog_bar=True, logger=True, sync_dist=True)
 
         # Additional classification metrics
-        y_class = torch.sigmoid(z_class)
+        #y_class = torch.sigmoid(z_class)
 
         class_score = y_class
         class_true = y.type(torch.int32)
@@ -111,7 +149,7 @@ class labCollabLM(pl.LightningModule):
         return optimizer
 
     def comp_loss(self, z_hat, y):
-        return F.binary_cross_entropy_with_logits(z_hat, y)
+        return F.binary_cross_entropy_with_logits(z_hat, y, reduction = 'mean')
 
 class labCollabDataset(BaseDataset):
     def __init__(
@@ -128,7 +166,7 @@ class labCollabDataset(BaseDataset):
         thisSeries = self.dataframe.loc[thisPatientMRN]
 
         if thisSeries.loc['IDA']:
-            diagnosisReelFrame = thisSeries.loc['reelframe']-random.randint(6,12)
+            diagnosisReelFrame = int(thisSeries.loc['reelFrame'])-random.randint(6,12)
         else:
             diagnosisReelFrame = random.randint(1,350)-random.randint(6,12)
 
@@ -140,9 +178,12 @@ class labCollabDataset(BaseDataset):
         selectedReel = np.zeros((52,12))
 
         patientReel = np.load('Data/lab_data_patientReels/'+str(thisPatientMRN)+'.npy')
+        patientReel = np.nan_to_num(patientReel)
         selectedReel[:, offset:] = patientReel[:,startFrame:endFrame]
+        selectedReel=np.transpose(selectedReel).astype(np.float32)
+        GTlabels = np.array([thisSeries.loc['IDA'].astype(np.float16)]) # pytorch lightning like this as a numpy array, size (batch, 1)
 
-        return selectedReel, thisSeries.loc['IDA']
+        return selectedReel, GTlabels
 
     def __len__(self):
         return len(self.patientList)
@@ -182,6 +223,7 @@ class labCollabDM(pl.LightningDataModule):
 
 if __name__ == "__main__":
     print(pl.__version__)
+    torch.set_float32_matmul_precision('medium')
     # load config file
     with open('100_config.yaml') as file:
         defaultConfigDict = yaml.safe_load(file)
@@ -201,18 +243,20 @@ if __name__ == "__main__":
     labCollabLightningModule = labCollabLM(args)
     labCollabDataModule = labCollabDM(args=args)
 
-    #wandb_logger = WandbLogger(project=args.projectName, name=timeStamp, tags=[args.tag])
-    wandb_logger = None
+    wandb_logger = WandbLogger(project=args.projectName, name=timeStamp, tags=[args.tag])
+    #wandb_logger = None
 
+    '''
     checkpoint_callback = ModelCheckpoint(dirpath='checkpoints/' + runName + '/',
                                           filename=runName + '-{epoch}-{valid_loss_total:.4f}',
                                           monitor='valid_loss_total',
                                           save_top_k=100,
                                           mode='min')
+    '''
 
-    trainer = pl.Trainer(logger=wandb_logger, log_every_n_steps=10, callbacks=[checkpoint_callback],
-                         accumulate_grad_batches=4,
+    trainer = pl.Trainer(logger=wandb_logger, log_every_n_steps=10,
+                         accumulate_grad_batches=10,
                          accelerator='gpu', devices=args.num_gpus, strategy='ddp_find_unused_parameters_false', precision = 16,
                          #accelerator='ddp', plugins=DDPPlugin(find_unused_parameters=False)],
                          max_epochs=args.max_epochs, num_sanity_val_steps=10)
-    trainer.fit(myRSNAcspineLightningModule, myRSNAcspineDataModule)
+    trainer.fit(labCollabLightningModule, labCollabDataModule)
--- 221217_GRUmodel.py
+++ 221217_GRUmodel.py
@@ -48,6 +48,32 @@ class BiGRU(nn.Module):
 
         return studyLevelOutputs
 
+class BiGRU(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(BiGRU, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.GRU = nn.GRU(
+            input_size, hidden_size, num_layers, batch_first=True, bidirectional=True
+        )
+        self.linear3 = nn.Linear(hidden_size * 2, hidden_size)
+        self.linear4 = nn.Linear(hidden_size, 1)
+
+    def forward(self, x):
+        #imageLevelOutputs = []
+        h0 = torch.zeros(self.num_layers * 2, self.hidden_size)
+        # c0 = torch.zeros(self.num_layers * 2, x.size(0), self.hidden_size).cuda()
+
+        out, h_n = self.GRU(x, h0)
+
+        print(h_n.size())
+        h_n = h_n.view(1, -1)
+        print(h_n.size())
+        studyLevelOutputs = F.relu(self.linear3(h_n))
+        studyLevelOutputs = self.linear4(studyLevelOutputs)
+
+        return studyLevelOutputs
+
 class BiGRUver2(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, num_classes):
         super(BiGRUver2, self).__init__()
@@ -65,7 +91,7 @@ class BiGRUver2(nn.Module):
     def forward(self, x):
         batch_size = x.size()[0]
 
-        h0 = torch.zeros(self.num_layers * 2, batch_size, self.hidden_size)
+        h0 = torch.zeros(self.num_layers * 2, batch_size, self.hidden_size).type(torch.FloatTensor)
         # c0 = torch.zeros(self.num_layers * 2, x.size(0), self.hidden_size).cuda()
 
         # Forward pass
@@ -79,17 +105,34 @@ class BiGRUver2(nn.Module):
 
         return studyLevelOutputs
 
+class simpleANN(nn.Module):
+    def __init__(self, sequence_size, input_size):
+        super(simpleANN, self).__init__()
+        self.linear1 = nn.Linear(sequence_size*input_size, 1024)
+        self.linear2 = nn.Linear(1024, 1)
+
+    def forward(self, x):
+        x = torch.flatten(x, start_dim=1, end_dim=-1)
+
+        # Forward pass
+        x = self.linear1(x)
+        x = self.linear2(x)
+
+        return x
+
 if __name__ == "__main__":
     INPUT_SIZE = 52
     HIDDEN_SIZE = 32
     NUM_LAYERS = 1
     NUM_CLASSES = 32
 
-    seq = BiGRUver2(INPUT_SIZE, HIDDEN_SIZE, NUM_LAYERS, NUM_CLASSES)
-
+    #seq = BiGRUver2(INPUT_SIZE, HIDDEN_SIZE, NUM_LAYERS, NUM_CLASSES)
+    seq = simpleANN(sequence_size=12, input_size=52)
     input = torch.rand((3, 12,52))
+    print(input.size())
     output = seq(input)
-    print(output)
+    print(output.size())
+
 
 
 
